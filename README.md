d6cdc40b7855afd6707fc41d82b97f1fbb66109f
Kyu Wang <hello@kyu.wang>
2023-05-08 15:12:06 +0800

    modify readme.md

--- README.md
+++ README.md
@@ -4,14 +4,25 @@ The project is for muggle, is for me.
 
 # Piu
 
+Piu is an open-source password manager application that supports API integration, stream data encryption, and two-factor authentication (2FA).
+
+With Piu, users can securely store and manage their passwords in an encrypted format. The API support allows easy integration with other applications and systems to provide seamless password management capabilities. The stream data encryption feature ensures the security of data even during network transfer. Piu allows users to retrieve 2FA codes directly from the app, making it even more convenient for users.
+
+Overall, Piu is a powerful and secure password management solution suitable for individuals and organizations alike.
+
 ## Dependencies
 
 - Go
 - Node.js
 - Yarn/Npm
 
-## Installation via Git
+## Installation via Git (source code)
 
 ```shell
-
+$ git clone https://github.com/kyukw/piu.git \
+    && cd piu \
+    && make get-golang \
+    && make get-nodejs \
+    && make build \
+    && make install
 ```
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,63 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Head from "next/head";
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+type Category = {
+  name: string;
+};
+
+type Categories = Array<Category>;
 
 export default function Home() {
+  const [showCategoryModal, setShowCategoryModal] = useState<boolean>(false);
+
+  const [categories, setCategory] = useState<Array<Category>>([
+    {
+      name: "账户",
+    },
+  ]);
+
+  console.log(categories);
+
   return (
-    <div>
-      <div className='max-w-[20rem]'>
-        <div className='flex justify-between'>
-          <h1>类别</h1>
-          <a href="">新建类别</a>
+    <>
+      <Head>
+        <title>Piu</title>
+      </Head>
+      <div>
+        <div className="max-w-[20rem] border-r">
+          <div className="min-h-screen bg-white">
+            <div className="py-2 px-3">
+              <div className="flex justify-between">
+                <h1>密码类型</h1>
+                <a
+                  onClick={() => setShowCategoryModal(!showCategoryModal)}
+                  className="text-blue-700"
+                  role="button"
+                >
+                  新建类别
+                </a>
+              </div>
+            </div>
+
+            {categories &&
+              categories.map(({ name }, key) => {
+                return (
+                  <div key={key}>
+                    <a href="">{name}</a>
+                  </div>
+                );
+              })}
+          </div>
         </div>
-
-        <a href="">登录</a>
-
       </div>
-    </div>
-  )
+
+      {showCategoryModal && (
+        <>
+          <div className="fixed min-h-screen min-w-full bg-opacity-20 animate-pulse bg-gray-700 top-0"></div>
+        </>
+      )}
+    </>
+  );
 }
